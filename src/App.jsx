920337d04c474551c6e0d172744f35373a668480
Breno Ribeiro <brenoribeiro.dev@gmail.com>
2025-08-25 23:23:36 -0300

    feat: add home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Logo from './assets/logo.svg';
+import './main.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="flex min-h-screen w-full flex-col justify-between">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-4 text-center">
+        <img src={Logo} alt="Logo mentorIA" />
+        <h1 className="mt-10 text-5xl font-extrabold">
+          Mentoria objetiva <br /> para a sua evolução profissional 🚀
+        </h1>
+        <p className="mt-2 text-sm font-light text-zinc-400">
+          Aguarde, estamos preparando tudo para você...
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </main>
+      <footer className="py-4 text-center text-zinc-400">
+        <p>© 2025 mentorIA. Todos os direitos reservados.</p>
+      </footer>
+    </div>
   );
 }
 
