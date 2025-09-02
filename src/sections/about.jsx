5308743128cf751dd43d538b5d2742fe642817b0
Bel <154826898+iambel@users.noreply.github.com>
2025-09-02 16:25:09 -0300

    :lipstick: style: update About section

---
+++ src/sections/about.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import robot from '../assets/robot.svg';
+
+const about = () => {
+  return (
+    <section className="relative flex h-screen items-center">
+      <div className="absolute top-0 left-0 h-40 w-full bg-[#2C49FA]"></div>
+
+      <div className="w-1/2 px-20">
+        <h1 className="pb-18 text-5xl font-extrabold text-[#3F3D56]">
+          Sobre o mentorIA
+        </h1>
+        <p className="pb-12 text-lg">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry. Lorem Ipsum has been
+          the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book.
+        </p>
+        <p className="text-lg">
+          orem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+      </div>
+
+      <img
+        src={robot}
+        alt="robot"
+        className="absolute -top-20 right-0 h-full"
+      />
+    </section>
+  );
+};
+
+export default about;
