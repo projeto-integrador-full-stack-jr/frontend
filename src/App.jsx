9cebbc93908bf278501b7eb41329a6ff33957a84
Breno Ribeiro <brenoribeiro.dev@gmail.com>
2025-09-12 20:45:21 -0300

    chore: install react-router-dom and setup initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './main.css';
 import LandingPage from './pages';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Routes>
+      <Route path="/inicio" element={<LandingPage />} />
+    </Routes>
+  );
 }
 
 export default App;
