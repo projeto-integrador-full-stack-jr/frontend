0bfb2b2edc602b231fcbf02312614f7971e49177
Breno Ribeiro <brenoribeiro.dev@gmail.com>
2025-10-19 16:18:25 -0300

    Remove API proxy configuration and add .env to .gitignore

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,4 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://app-mentori-ia-prod.azurewebsites.net/',
-                changeOrigin: true,
-                secure: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
-    },
 });
